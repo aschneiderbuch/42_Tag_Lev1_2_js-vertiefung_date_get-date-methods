7185e5abf34e11f49936f27afb0726a8d95843bf
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-09 12:35:48 +0100

    htmlGerüst

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+console.log("test");
+
+/* 
+Ausgabe
+    Fri
+    03
+    31
+    27
+    PM
+
+*/
+
+
+let monate = [
+    "Januar",
+    "Februar",
+    "März",
+    "April",
+    "Mai",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "Dezember"
+  ];
+  
+  let wochenTag = [
+    "Sonntag",
+    "Montag",
+    "Dienstag",
+    "Mittwoch",
+    "Donnerstag",
+    "Freitag",
+    "Samstag"
+  ];
+  
